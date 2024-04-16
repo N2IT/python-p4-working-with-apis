0b7b27609988b9061b553d203fdd89a9a2fc20a6
Tony Eder <tonyeder11@gmail.com>
2024-04-16 14:46:57 -0700

    had to use different endpoint as the one suggested within the lesson does not allow access.

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -5,10 +5,27 @@ import json
 class GetPrograms:
 
   def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+    URL = "https://data.cityofnewyork.us/resource/w7w3-xahh.json"
 
     response = requests.get(URL)
     return response.content
 
-programs = GetPrograms().get_programs()
-print(programs)
+  def business_industries(self):
+    # we use the JSON library to parse the API response into nicely formatted JSON
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+        programs_list.append(program["industry"])
+
+    return programs_list
+  
+programs = GetPrograms()
+industries = programs.business_industries()
+
+for industry in set(industries):
+  print(industry)
+
+
+
+# programs = GetPrograms().get_programs()
+# print(programs)
